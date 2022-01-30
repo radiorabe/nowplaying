bd51158cb0f5aac94096997a2bc0da4ae2ef4094
Lucas Bickel <hairmare@rabe.ch>
2022-01-31 00:07:18 +0100

    feat: opentelemetry logging
    - for formating and whatnot
    - in preparation for more instrumentation later on

--- nowplaying/main.py
+++ nowplaying/main.py
@@ -1,9 +1,8 @@
-import logging
 import socket
-import sys
 
 from .daemon import NowPlayingDaemon
 from .options import Options
+from .otel import setup_otel
 
 
 class NowPlaying:
@@ -12,31 +11,17 @@ class NowPlaying:
         self.options = Options()
         self.options.parse_known_args()
 
-        self.setup_logging()
+        self._setup_otel()
         socket.setdefaulttimeout(self.options.socketDefaultTimeout)
         self._run_daemon()
+
+    def _setup_otel(self):  # pragma: no cover
+        if not self.options.debug:
+            setup_otel(self.options.otlp_enable)
 
     def _run_daemon(self):
         """Start nowplaying daemon."""
         NowPlayingDaemon(self.options).main()
-
-    def setup_logging(self):
-        """Configure logging to stdout."""
-        root = logging.getLogger()
-        root.setLevel(logging.DEBUG)
-
-        stdout_handler = logging.StreamHandler(sys.stdout)
-
-        stdout_formatter = logging.Formatter(
-            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-        )
-
-        stdout_handler.setLevel(logging.INFO)
-        if self.options.debug:
-            stdout_handler.setLevel(logging.DEBUG)
-        stdout_handler.setFormatter(stdout_formatter)
-
-        root.addHandler(stdout_handler)
 
 
 if __name__ == "__main__":  # pragma: no cover
