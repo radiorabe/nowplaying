5ec451edd648523d8c92d5808dd883feb8ec8c54
Lucas Bickel <116588+hairmare@users.noreply.github.com>
2024-03-11 02:10:52 +0100

    chore(ci): configure ruff

--- nowplaying/track/observers/ticker.py
+++ nowplaying/track/observers/ticker.py
@@ -1,15 +1,24 @@
+"""TickerTrackObserver generates the songticker.xml file."""
+
+from __future__ import annotations
+
 import datetime
 import logging
 import uuid
 import warnings
+from typing import TYPE_CHECKING, Self
 
-import configargparse
-import isodate
-import lxml.builder
+import isodate  # type: ignore[import-untyped]
+import lxml.builder  # type: ignore[import-untyped]
 import lxml.etree
 import pytz
 
-from .base import TrackObserver
+from nowplaying.track.observers.base import TrackObserver, TTrackObserverOptions
+
+if TYPE_CHECKING:  # pragma: no cover
+    import configargparse  # type: ignore[import-untyped]
+
+    from nowplaying.track.track import Track
 
 logger = logging.getLogger(__name__)
 
@@ -28,7 +37,11 @@ class TickerTrackObserver(TrackObserver):
         """TickerTrackObserver options."""
 
         @classmethod
-        def args(cls, args: configargparse.ArgParser) -> None:
+        def args(
+            cls: type[TTrackObserverOptions],
+            args: configargparse.ArgParser,
+        ) -> None:
+            """Build args."""
             args.add_argument(
                 "--xml-output",
                 dest="tickerOutputFile",
@@ -36,19 +49,25 @@ class TickerTrackObserver(TrackObserver):
                 default="/var/www/localhost/htdocs/songticker/0.9.3/current.xml",
             )
 
-        def __init__(self, file_path: str):
+        def __init__(self: Self, file_path: str) -> None:
+            """Create TickerTrackObserver.Config."""
             self.file_path = file_path
 
-    def __init__(self, options: Options):
+    def __init__(self: Self, options: Options) -> None:
+        """Create TickerTrackObserver."""
         warnings.warn(
             "The XML ticker format will be replaced with a JSON variant in the future",
             PendingDeprecationWarning,
+            stacklevel=2,
         )
         self.ticker_file_path = options.file_path
 
-    def track_started(self, track):
+    def track_started(self: Self, track: Track) -> None:
+        """Track started."""
         logger.info(
-            f"Updating Ticker XML file for track: {track.artist} - {track.title}"
+            "Updating Ticker XML file for track: %s - %s",
+            track.artist,
+            track.title,
         )
         try:
             tz = pytz.timezone("Europe/Zurich")
@@ -59,11 +78,11 @@ class TickerTrackObserver(TrackObserver):
 
         now = isodate.datetime_isoformat(datetime.datetime.now(tz))
 
-        MAIN_NAMESPACE = "http://rabe.ch/schema/ticker.xsd"
-        XLINK_NAMESPACE = "http://www.w3.org/1999/xlink"
-        XLINK = "{%s}" % XLINK_NAMESPACE
+        MAIN_NAMESPACE = "http://rabe.ch/schema/ticker.xsd"  # noqa: N806
+        XLINK_NAMESPACE = "http://www.w3.org/1999/xlink"  # noqa: N806
+        XLINK = "{%s}" % XLINK_NAMESPACE  # noqa: N806, UP031
 
-        E = lxml.builder.ElementMaker(
+        E = lxml.builder.ElementMaker(  # noqa: N806
             namespace=MAIN_NAMESPACE,
             nsmap={None: MAIN_NAMESPACE, "xlink": XLINK_NAMESPACE},
         )
@@ -73,17 +92,17 @@ class TickerTrackObserver(TrackObserver):
         show_ref.attrib[XLINK + "show"] = "replace"
 
         ticker = E.ticker(
-            E.identifier("ticker-%s" % uuid.uuid4()),
+            E.identifier(f"ticker-{uuid.uuid4()}"),
             E.creator("now-playing daemon v2"),
             E.date(now),
             E.show(
                 E.name(track.show.name),
                 show_ref,
                 E.startTime(
-                    isodate.datetime_isoformat(track.show.starttime.astimezone(tz))
+                    isodate.datetime_isoformat(track.show.starttime.astimezone(tz)),
                 ),
                 E.endTime(
-                    isodate.datetime_isoformat(track.show.endtime.astimezone(tz))
+                    isodate.datetime_isoformat(track.show.endtime.astimezone(tz)),
                 ),
                 id=track.show.uuid,
             ),
@@ -103,5 +122,5 @@ class TickerTrackObserver(TrackObserver):
             encoding="utf-8",
         )
 
-    def track_finished(self, track):
-        return True
+    def track_finished(self: Self, _: Track) -> None:
+        """Track finished."""
